229ef364778e8eba606def955611b0a5f80419f5
Manuel Duran <mduran85@gmail.com>
2020-04-12 11:04:16 -0500

    set up guessed words reducer

--- base-for-hooks-context/src/actions/index.test.js
+++
@@ -1,8 +0,0 @@
-import { correctGuess, actionTypes } from './index';
-
-describe('correctGuess', () => {
-  test('returns an action with type "correct guess"', () => {
-    const action = correctGuess();
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS })
-  })
-})
---
+++ base-for-hooks-context/src/integration.test.js
@@ -0,0 +1,21 @@
+import { storeFactory } from '../test/testUtils';
+import { guessedWord } from './actions';
+
+describe('guessedWord action dispatcher', () => {
+  describe('no guessed words', () => {
+    test('updates state correctly for unsuccesful guess', () => {
+
+    })
+    test('updates state correctly for succesful guess', () => {
+
+    })
+  })
+  describe('some guessed words', () => {
+    test('updates state correctly for unsuccesful guess', () => {
+
+    })
+    test('updates state correctly for succesful guess', () => {
+
+    })
+  })
+})
